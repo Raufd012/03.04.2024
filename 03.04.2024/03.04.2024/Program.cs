96a4c051d90423409984a0be5b1a5671551d31b3
Rauf <161576550+Raufd012@users.noreply.github.com>
2024-04-03 19:58:10 +0400

    initial commit

---
+++ 03.04.2024/03.04.2024/Program.cs
@@ -0,0 +1,19 @@
+﻿
+using _03._04._2024.Models;
+
+namespace _03._04._2024
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Employee employee = new Employee("Rauf", false, 350m);
+
+            Assistant assistant = new Assistant();
+
+            Manager manager = new Manager();
+
+            assistant.GetFeedback(employee);
+        }
+    }
+}
